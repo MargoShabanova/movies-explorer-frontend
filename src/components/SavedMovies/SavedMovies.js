a779bbf293f9ea0b1a01ffbf30330b5432618bcb
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-11-17 19:57:54 +0300

    feat: add search

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,10 +1,12 @@
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import "./SavedMovies.css";
 import { savedMovies } from "../../utils/constants";
+import SearchForm from "../SearchForm/SearchForm";
 
 export default function SavedMovies() {
   return (
     <main className="saved-movies">
+      <SearchForm />
       <MoviesCardList cards={savedMovies}/>
     </main>
   );
